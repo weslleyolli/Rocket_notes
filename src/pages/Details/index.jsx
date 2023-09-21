c36fe41e46d5f9e214f69d69d306728b4cf2abc9
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-21 12:30:34 -0300

    children

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,14 +1,26 @@
-import { Container } from"./styles.js";
+import { Container, Links } from"./styles.js";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
+import { Section } from "../../components/Section";
 
 export function Details() {
   return (
     <Container>
         <Header />
 
-          <Button title="voltar" />
+        <Section title="Utils links">
+          <Links >
+            <li>
+              <a href="#">https://www.rocketseat.com.br/</a>
+            </li>
+            <li>
+              <a href="#">https://www.rocketseat.com.br/</a>
+            </li>
+          </Links>
+        </Section>
+        
+        <Button title="voltar" />
     </Container>
 
   )
