1003c8995baf6fc862264b2e35bd348bfc05fbfe
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-22 11:35:03 -0300

    applying grid in Home

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import  GlobalStyles  from './styles/global'
 
 import theme from './styles/theme.js'
 
-import { Details } from './pages/Details/index.jsx'
+import { Home } from './pages/Home/index.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Details />
+      <Home />
     </ThemeProvider>
   </React.StrictMode>,
 )
