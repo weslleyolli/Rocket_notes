cd96013f382e7ce8472ef75b10c32130afcb4fa1
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-22 10:33:45 -0300

    tag component

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,26 +1,32 @@
-import { Container, Links } from"./styles.js";
+import { Container, Links } from "./styles.js";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
 import { Section } from "../../components/Section";
+import { Tag } from "../../components/Tag";
 
 export function Details() {
   return (
     <Container>
-        <Header />
+      <Header />
 
-        <Section title="Utils links">
-          <Links >
-            <li>
-              <a href="#">https://www.rocketseat.com.br/</a>
-            </li>
-            <li>
-              <a href="#">https://www.rocketseat.com.br/</a>
-            </li>
-          </Links>
-        </Section>
-        
-        <Button title="voltar" />
+      <Section title="Utils links">
+        <Links >
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+          <li>
+            <a href="#">https://www.rocketseat.com.br/</a>
+          </li>
+        </Links>
+      </Section>
+
+      <Section title="Markers">
+        <Tag title="express" />
+        <Tag title="notejs" />
+      </Section>
+
+      <Button title="voltar" />
     </Container>
 
   )
