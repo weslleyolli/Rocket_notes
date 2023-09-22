c7adf12a98ac5ff8ce7ae2e9d171b8884827effa
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-22 11:02:52 -0300

    Finishing Details

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,4 +1,4 @@
-import { Container, Links } from "./styles.js";
+import { Container, Links, Content } from "./styles.js";
 
 import { Button } from "../../components/Button";
 import { Header } from "../../components/Header";
@@ -11,25 +11,40 @@ export function Details() {
     <Container>
       <Header />
 
-      <ButtonText title="Delete notes"/>
+      <main>
+        <Content>
+          <ButtonText title="Delete notes" />
+          
+          <h1>
+            Introduction to react
+          </h1>
 
-      <Section title="Utils links">
-        <Links >
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-          <li>
-            <a href="#">https://www.rocketseat.com.br/</a>
-          </li>
-        </Links>
-      </Section>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Incidunt ab corporis qui maxime fuga unde minima dolorem vero
+            laborum. Eligendi fugiat excepturi placeat, ad quaerat unde 
+            mollitia dolorem ab suscipit?
+          </p>
+          <Section title="Utils links">
+            <Links >
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+              <li>
+                <a href="#">https://www.rocketseat.com.br/</a>
+              </li>
+            </Links>
+          </Section>
 
-      <Section title="Markers">
-        <Tag title="express" />
-        <Tag title="notejs" />
-      </Section>
+          <Section title="Markers">
+            <Tag title="express" />
+            <Tag title="notejs" />
+          </Section>
 
-      <Button title="voltar" />
+          <Button title="voltar" />
+        </Content>
+      </main>
+
     </Container>
 
   )
