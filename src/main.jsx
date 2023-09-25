2b16669b88f8dfbbb66f9332c06852ed45c8d577
Weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-25 10:19:24 -0300

    structure of SignIn

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import  GlobalStyles  from './styles/global'
 
 import theme from './styles/theme.js'
 
-import { Home } from './pages/Home/index.jsx'
+import { SignIn } from './pages/SignIn'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <SignIn />
     </ThemeProvider>
   </React.StrictMode>,
 )
