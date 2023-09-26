30131e5410f2e2781d72d128dbc1e51e33e91ced
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-26 20:01:18 -0300

    user registration

--- src/pages/SignUp/index.jsx
+++ src/pages/SignUp/index.jsx
@@ -1,6 +1,8 @@
+import { useState } from 'react'
 import { FiMail, FiLock, FiUser } from 'react-icons/fi'
+import { Link, useNavigate } from 'react-router-dom'
 
-import { Link } from 'react-router-dom'
+import { api } from "../../services/api"
 
 import { Input } from '../../components/Input'
 import { Button } from '../../components/Button'
@@ -8,6 +10,31 @@ import { Button } from '../../components/Button'
 import { Container, Form, Background } from './styles'
 
 export function SignUp() {
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+
+    const navigate = useNavigate()
+
+    function handleSignUp(){
+        if( !name || !email || !password) {
+            return alert("Please enter all fields")
+        }
+
+        api.post("/users", {name, email, password})
+        .then(() => {
+            alert("Success")
+            navigate("/")
+        })
+        .catch(err => {
+            if(err.response){
+                alert(err.response.data.message)
+            }else {
+                alert("dont was possible register")
+            }
+        })
+    }
+
     return (
         <Container >
             <Background />
@@ -21,20 +48,24 @@ export function SignUp() {
                     placeholder="Name"
                     type="text"
                     icon={FiUser}
+                    onChange={e => setName(e.target.value)}
                 />
 
                 <Input
                     placeholder="Email"
                     type="text"
                     icon={FiMail}
+                    onChange={e => setEmail(e.target.value)}
                 />
                 <Input
                     placeholder="Password"
                     type="password"
                     icon={FiLock}
+                    onChange={e => setPassword(e.target.value)}
+                    
                 />
 
-                <Button title="Register" />
+                <Button title="Register" onClick={handleSignUp} />
 
                 <Link to="/">
                     Return to login
