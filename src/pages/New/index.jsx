c9360023f461f0ea2cf74702812e23d634057054
weslleyolli <weslleyrichardson.cg@hotmail.com>
2023-09-25 22:16:31 -0300

    news header

--- src/pages/New/index.jsx
+++ src/pages/New/index.jsx
@@ -1,10 +1,22 @@
 import { Header } from '../../components/Header'
-import { Container } from './styles'
+import { Input } from '../../components/Input'
+import { Container, Form } from './styles'
 
 export function New() {
     return (
         <Container>
             <Header />
+
+            <main>
+                <Form>
+                    <header>
+                        <h1>Create Note</h1>
+                        <a href="#">back</a>
+                    </header>
+
+                    <Input placeholder="Title"/>
+                </Form>
+            </main>
         </Container>
     )
 }
