62a3afa5a6f48d7f4b715af729f2c08cecf7023a
weslleyolli <weslleyrichardson.cg@hotmail.com>
2024-09-22 11:54:43 -0300

    screen mobile: page home ok

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -1,5 +1,5 @@
-import styled from "styled-components"
-import { Link } from "react-router-dom"
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 export const Container = styled.div`
     width: 100%;
@@ -14,55 +14,80 @@ export const Container = styled.div`
     "menu content"
     "newnote content";
 
-    background-color:${({ theme }) => theme.COLORS.BACKGROUND_800} ;
-`
+    background-color: ${({ theme }) => theme.COLORS.BACKGROUND_800};
+
+    @media (max-width: 768px) {
+        grid-template-columns: 1fr;
+        grid-template-rows: 105px auto 1fr 64px;
+        grid-template-areas: 
+        "header"
+        "search"
+        "content"
+        "newnote";
+
+        overflow-y: auto;
+    }
+`;
 
 export const Brand = styled.div`
     grid-area: brand;
     display: flex;
     justify-content: center;
     align-items: center;
-    border-bottom:  1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
-
+    border-bottom: 1px solid ${({ theme }) => theme.COLORS.BACKGROUND_700};
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
 
     > h1 {
         font-size: 24px;
         color: ${({ theme }) => theme.COLORS.ORANGE};
     }
-`
+
+    @media (max-width: 768px) {
+        display: none; // Esconde o brand no mobile para dar mais espaço ao conteúdo
+    }
+`;
 
 export const Menu = styled.ul`
     grid-area: menu;
     background-color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-
     padding-top: 64px;
     text-align: center;
 
     > li {
         margin-bottom: 24px;
     }
-`
+
+    @media (max-width: 768px) {
+        display: none; // Esconde o menu no mobile para simplificar a interface
+    }
+`;
 
 export const Search = styled.div`
     grid-area: search;
-
     padding: 64px 64px 0;
-    
-`
+
+    @media (max-width: 768px) {
+        padding-left: 12px;
+        padding-top: 12px;
+        padding-right: 12px; // Reduz o padding no mobile para um layout mais compacto
+    }
+`;
+
 export const Content = styled.div`
     grid-area: content;
-
     padding: 0 64px;
     overflow-y: auto;
-`
+
+    @media (max-width: 768px) {
+        padding-left: 16px;
+        padding-right: 16px; // Reduz o padding lateral no mobile
+    }
+`;
+
 export const NewNote = styled(Link)`
     grid-area: newnote;
-
     background-color: ${({ theme }) => theme.COLORS.ORANGE};
     color: ${({ theme }) => theme.COLORS.BACKGROUND_900};
-
-
     display: flex;
     align-items: center;
     justify-content: center;
@@ -70,5 +95,4 @@ export const NewNote = styled(Link)`
     svg {
         margin-right: 8px;
     }
-
-`
+`;
